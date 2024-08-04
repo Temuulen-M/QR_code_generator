2ef2f08889dc53e2a049b70033599f009538d967
Temuulen <1temuulenmolom@gmail.com>
2024-08-04 15:04:32 +0200

    Barebones but functional

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import qrcode as qr
+import qrcode
 
+def generate_qr(text, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save(file_name)
+
+
+def main():
+    data = input("QR code data:")
+    generate_qr(data, "generated qr code.png")
+
+if __name__ == "__main__":
+    main()
